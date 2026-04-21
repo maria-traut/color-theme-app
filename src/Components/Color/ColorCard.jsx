7f3cc0124d6be59d3453a38a2b61590b6c3b0a18
Maria <mia.gotcha@proton.me>
2026-04-21 16:45:39 +0200

    shorten style attributes in ColoCard

--- src/Components/Color/ColorCard.jsx
+++ src/Components/Color/ColorCard.jsx
@@ -2,19 +2,13 @@ import "./ColorCard.css";
 
 export default function ColorCard({ color }) {
   return (
-    <article className="colorcard" style={{ backgroundColor: color.hex }}>
-      <p className="colorcard__value" style={{ color: color.contrastText }}>
-        {color.hex}
-      </p>
-      <h3 className="colorcard__role" style={{ color: color.contrastText }}>
-        {color.role}
-      </h3>
-      <p
-        className="colorcard__contrastText"
-        style={{ color: color.contrastText }}
-      >
-        contrast: {color.contrastText}
-      </p>
+    <article
+      className="colorcard"
+      style={{ backgroundColor: color.hex, color: color.contrastText }}
+    >
+      <p className="colorcard__value">{color.hex}</p>
+      <h3 className="colorcard__role">{color.role}</h3>
+      <p className="colorcard__contrastText">contrast: {color.contrastText}</p>
     </article>
   );
 }
